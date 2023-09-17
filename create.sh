ad6513f022e02b174f56b58a98cb73813213b54a
Umiiiiiiii <144937107+Umiiiiiiii@users.noreply.github.com>
2023-09-17 19:08:44 +0800

    Update create.sh

--- create.sh
+++ create.sh
@@ -4,14 +4,4 @@
 message="Hello World"
 
 # 打印消息
-echho $message
-
-# 读取用户输入的数字
-read -p "请输入一个数字：" number
-
-# 判断数字是否大于10
-if [$number > 10]; then
-  echo "该数字大于10"
-else
-  echo "该数字小于或等于10"
-fi
+echo $message
